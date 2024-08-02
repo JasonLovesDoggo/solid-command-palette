09681cfb0750ed16e27849d3c9c7e00fa311a2b2
Jason <git@jasoncameron.dev>
2024-08-01 20:56:56 -0400

    convert to solid router

--- src/app/views/docs/Api/ApiDefineAction.view.tsx
+++ src/app/views/docs/Api/ApiDefineAction.view.tsx
@@ -1,20 +1,20 @@
 import { Component } from 'solid-js';
-import { Link } from 'solid-app-router';
 import { Snippet } from '../Snippet/Snippet';
 import docsStyles from '../docsUtils.module.css';
+import {A} from "@solidjs/router";
 
 const ApiRootView: Component = () => {
-  return (
-    <section class={docsStyles.section}>
-      <h2>defineAction</h2>
-      <p class={docsStyles.text}>It helps you define an action with autocomplete suggestions.</p>
-      <p class={docsStyles.text}>
-        Related actions can be grouped in modules and exported. The combined map of actions is
-        passed to <Link href="/docs/api/root">Root</Link>
-      </p>
-      <Snippet snippetId="api-define-action" />
-    </section>
-  );
+    return (
+        <section class={docsStyles.section}>
+            <h2>defineAction</h2>
+            <p class={docsStyles.text}>It helps you define an action with autocomplete suggestions.</p>
+            <p class={docsStyles.text}>
+                Related actions can be grouped in modules and exported. The combined map of actions is
+                passed to <A href="/docs/api/root">Root</A>
+            </p>
+            <Snippet snippetId="api-define-action"/>
+        </section>
+    );
 };
 
 export default ApiRootView;
